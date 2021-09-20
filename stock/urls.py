2acc1f747358273518f1c5309da8ec275cc71898
Somphoph <somphop.homprasert@krungthai.com>
2021-09-20 18:14:02 +0700

    Update edit receive order and fix product list not display.

--- stock/urls.py
+++ stock/urls.py
@@ -5,9 +5,9 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('receive/<int:receive_id>', views.receive_edit, name='edit'),
-    path('receive/', views.receive_add, name='add'),
-    path('receive/details/',views.receive_detail_add,name="detail_add"),
-    path('receive/details/<int:detail_id>',views.receive_detail_edit,name="detail_edit"),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+                  path('', views.IndexView.as_view(), name='index'),
+                  path('receive/<int:receive_id>', views.receive_edit, name='edit'),
+                  path('receive/', views.receive_add, name='add'),
+                  path('receive/details/', views.receive_detail_add, name="detail_add"),
+                  path('receive/details/<int:detail_id>', views.receive_detail_edit, name="detail_edit"),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
